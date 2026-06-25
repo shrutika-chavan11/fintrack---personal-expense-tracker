d413f4b2b74265fb4449e3914fd5f8f2aa9ab9f9
Shrutika Chavan <shrutikac11@gmail.com>
2026-06-25 18:53:15 +0530

    Initial FinTrack release

--- fintrack-backend/src/config/db.js
+++ fintrack-backend/src/config/db.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
@@ -7,8 +8,13 @@ dotenv.config();
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    ca: fs.readFileSync("./ca.pem"),
+  },
 });
 
 export default connection;
